f2c3756cf83a21c400ba8cc4c6d831021d67c0d2
Github <sasinmaharjan96>
2019-10-20 15:58:58 +0545

    Initial commit

---
+++ app/src/main/java/com/example/viewimage/LifeCycle.java
@@ -0,0 +1,27 @@
+package com.example.viewimage;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+
+public class LifeCycle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle);
+
+        Log.d("msg","onresume");
+        onPause();
+
+
+    }
+
+}
